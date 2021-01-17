f03347193f229b22ec7125b653023688e0250ce5
werro40 <37347053+werro40@users.noreply.github.com>
2021-01-16 19:46:52 -0700

    mess

--- Source/MainBonk.py
+++ Source/MainBonk.py
@@ -5,7 +5,46 @@ Created on Sat Jan 16 07:06:12 2021
 @author: adila
 """
 
-import numpy as np
-import tensorflow as tf
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 16 08:54:13 2021
 
-tf.Tensor(np.array([np.pi]))
+@author: adila
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+from flask import Flask, redirect, url_for, render_template, request
+
+
+def plotter(day):
+    return day;
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+        return render_template("index.html")
+    
+@app.route("/bonkground")
+def bonkground():
+        return render_template("bonkground.html")
+
+@app.route("/bonker", methods=["POST","GET"])
+def bonker():
+    if request.method == "POST":
+        days = request.form["nm"]
+        plotter(days)
+        return redirect(url_for("bonker"))
+    else:
+        return render_template("bonker.html")
+
+#@app.route("/<usr>", methods=["POST","GET"])
+# user(usr):
+#        return f"<h1>{usr}</h1>"
+
+if __name__ == "__main__":
+    app.run()
+
+
---
+++ Source/plotter.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 16 19:37:34 2021
+
+@author: adila
+"""
+
+def plotter():
+    return 0;
